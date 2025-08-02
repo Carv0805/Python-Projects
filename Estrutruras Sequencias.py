41d4b5684808cd454203f66d49b895e53febb2b9
VItor Carvalho de Araujo <105469574+Carv0805@users.noreply.github.com>
2025-08-02 20:00:13 -0300

    Add files via upload

---
+++ Estrutruras Sequencias.py
@@ -0,0 +1,99 @@
+#Ex 1
+#print('Alo Mundo!')
+
+#Ex 2
+#n1= int(input('Digite um número: '))
+#print('O número informado foi: ', n1)
+
+#Ex 3
+#n1= float(input('Digite um número: '))
+#n2= float(input('Digite um número: '))
+#n3 = n1+n2
+#print("A soma dos dois numeros é:" , n3)
+
+#Ex 4
+#n1 = float(input("Digite a nota do 1° Bimestre:"))
+#n2 = float(input("Digite a nota do 2° Bimestre:"))
+#n3 = float(input("Digite a nota do 3° Bimestre:"))
+#n4 = float(input("Digite a nota do 4° Bimestre:"))
+#media = (n1 + n2 + n3 + n4) / 4
+#print("A média das notas é: " , media)
+
+#Ex 5
+#m = float(input("Digite um valor de comprimento em metros: "))
+#cm = m * 100 
+#print("O valor que voce digitou em centimetros é escrito desta forma: ", cm)
+
+#Ex 6
+#raio = float(input("Digite o raio do circulo: "))
+#area = (raio**2) * 3.14
+#print("A area do circulo é: ", area)
+
+# #Ex 7
+# area = float(input("Digite o valor da area: "))
+# dobro_area = area * 2
+# print("O dobro da area é: ", dobro_area)
+
+#Ex 8
+#dinheiro_hora = float(input("Digite o valor que voce ganha por hora: "))
+#hora = float(input("Digite o números de horas que você trabalhou este mês: "))
+#salario_total = dinheiro_hora * hora
+#print("O valor seu salario mensal é: ", salario_total)
+
+#Ex 9
+#f = float(input("Digite a temperatura atual em graus Farenheit: "))
+#c = (5*(f-32)/9)
+#print("A temperatura que voce digitou em Farenheit convertida em graus Celsius é: ", c)
+
+#Ex 10
+#c = float(input("Digite a temperatura atual em graus Celsius: "))
+#f = ((c*9)/5) + 32
+#print("A temperatura que voce digitou em Celsius convertida em graus Farenheit é: ", f)
+
+#Ex 11
+#n1 = int(input("Digite um núnero inteiro: "))
+#n2 = int(input("Digite outro número inteiro: "))
+#n3 = float(input("Digite um número real: "))
+
+#r1= (2 * n1) * (n2 / 2)
+#r2 = (3 * n1) + n3
+#r3 = n3 ** 3
+
+#print(r1, r2 , r3)
+
+#Ex 12
+#altura = float(input("Digite sua altura: "))
+#peso_ideal = (72.7 * altura) - 58
+#print("O seu peso ideal com base na sua altura é: ", peso_ideal)
+
+#Ex 13
+#altura = float(input("Digite sua altura: "))
+#peso_ideal_h = (72.7 * altura) - 58
+#peso_ideal_m = (62.5 * altura) - 44.7
+#print("O seu peso ideal com base na sua altura se voce for homem é: ", peso_ideal_h , "\n" "O seu peso ideal com base na sua altura se voce seja mulher é: ", peso_ideal_m)
+
+#Ex 14
+#peso_peixes = float(input("Digite o peso dos peixes em kg: "))
+#excesso = peso_peixes - 50.0
+#multa = excesso * 4.0
+#print("Voce excedeu ", excesso, "kg. Logo a multa que vcoe deve pagar é de ", multa)
+
+#Ex 15
+#dinheiro_hora = float(input("Digite o valor que voce ganha por hora: "))
+#hora = float(input("Digite o números de horas que você trabalhou este mês: "))
+#salario = dinheiro_hora * hora
+#ir = salario * 0.11
+#inss = salario * 0.08
+#sindicato = salario * 0.05
+#liquido = salario - ir - inss - sindicato
+#print("O valor seu salario bruto é: ", salario)
+#print( "Voce pagou ", inss , " de INSS.")
+#print("Voce pagou ", sindicato , " de para o Sindicato.")
+#print("O seu salario liquido é ", liquido)
+
+#Ex 16
+
+#Ex 17
+
+#Ex 18
+
